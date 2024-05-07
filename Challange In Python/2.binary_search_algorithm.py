6743efdb11ec104876c4d1a65785c3f62df2feed
Valentine Omonya <valomosh254@gmail.com>
2024-05-07 17:05:46 +0300

    Corrected binary search fle

--- Challange In Python/2.binary_search_algorithm.py
+++ Challange In Python/2.binary_search_algorithm.py
@@ -1,17 +1,3 @@
-# def binary_search(arr, target):
-#     if len(arr) == 0:
-#         return -1
-#     midpoint = (len(arr)//2)
-#     if arr[midpoint] == target:
-#         return arr.index(target)
-#     elif arr[midpoint] < target:
-#         split_list = arr[midpoint+1:]
-#         return binary_search(split_list, target)
-#     elif arr[midpoint] > target:
-#         split_list = arr[:midpoint]
-#         return binary_search(split_list, target)
-
-
 num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
